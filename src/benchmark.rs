9b9b0cea0ce40aa574a993c6289c6c0ee50cd707
David Pedersen <david.pdrsn@gmail.com>
2017-11-23 14:23:55 +0100

    Add benchmarking of parsing small strings
    So we don't accidentally optimize only for parsing very large strings

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -5,6 +5,11 @@ use std::time::{Instant, Duration};
 
 fn main() {
     let string_with_hashtags = "#rust is #awesome";
+    benchmark_small_string(&string_with_hashtags);
+    benchmark_big_string(&string_with_hashtags);
+}
+
+fn benchmark_big_string(string_with_hashtags: &str) {
     let mut buffer = String::new();
     let strings_to_join = 10_000_000;
     for _ in 0..strings_to_join {
@@ -15,7 +20,7 @@ fn main() {
     let iterations = 10;
     let times: Vec<Duration> = (0..iterations)
         .map(|i| {
-            println!("Started parsing round {}", i + 1);
+            println!("{} / {}", i + 1, iterations);
             let start = Instant::now();
             let hashtags = Hashtag::parse(&buffer);
             let count = hashtags.len();
@@ -27,10 +32,28 @@ fn main() {
 
     let total: Duration = times.iter().sum();
     let avg: Duration = total / (times.len() as u32);
-
-    println!("Iterations: {}", iterations);
-    println!("Average: {:?}", avg);
+    println!("Big string average: {:?}", avg);
 }
 
-// Baseline
-// Duration { secs: 4, nanos: 248856955 }
+fn benchmark_small_string(string_with_hashtags: &str) {
+    let iterations = 10_000_000;
+
+    let times: Vec<_> = (0..iterations)
+        .map(|i| {
+            if (i % 1_000_000) == 0 {
+                println!("{} / {}", (i / 1_000_000) + 1, iterations / 1_000_000);
+            }
+
+            let start = Instant::now();
+            let hashtags = Hashtag::parse(&string_with_hashtags);
+            let count = hashtags.len();
+            let duration = start.elapsed();
+            assert_eq!(count, 2);
+            duration
+        })
+        .collect();
+
+    let total: Duration = times.iter().sum();
+    let avg: Duration = total / (times.len() as u32);
+    println!("Small string average: {:?}", avg);
+}
