bf8c97e070b5ae0e72819ca36e767e467fc71f61
David Pedersen <david.pdrsn@gmail.com>
2021-02-14 15:13:53 +0100

    Bump version to 1.0.1

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,7 @@
     unused_qualifications,
     unknown_lints
 )]
-#![doc(html_root_url = "https://docs.rs/hashtag/1.0.0")]
+#![doc(html_root_url = "https://docs.rs/hashtag/1.0.1")]
 
 use std::{
     borrow::Cow,
